56c75c47ef4c13f4c76463f6c61de2a15a9cc008
minsoo.lee <preinpost@gmail.com>
2023-03-11 18:07:03 +0900

    add cpus api

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ use sysinfo::{CpuExt, System, SystemExt};
 #[tokio::main]
 async fn main() {
     let router = Router::new()
-        .route("/", get(root_get)).with_state(AppState {
+        .route("/", get(root_get))
+        .route("/api/cpus", get(cpus_get))
+        .with_state(AppState {
         sys: Arc::new(Mutex::new(System::new())),
     });
 
@@ -27,7 +29,11 @@ struct AppState {
     sys: Arc<Mutex<System>>
 }
 
-async fn root_get(State(state): State<AppState>) -> String {
+async fn root_get() -> &'static str {
+    "Hello"
+}
+
+async fn cpus_get(State(state): State<AppState>) -> String {
     use std::fmt::Write;
 
     let mut s = String::new();
