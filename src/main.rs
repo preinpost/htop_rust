6331dad9b6840335f9a4b9215284a747c954d4c9
minsoo.lee <preinpost@gmail.com>
2023-03-11 21:30:03 +0900

    add dynamic html

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 use axum::{Json, Router, Server};
 use axum::extract::State;
-use axum::response::{Html, IntoResponse};
+use axum::response::{Html, IntoResponse, Response};
 use axum::routing::get;
 use sysinfo::{CpuExt, System, SystemExt};
 
@@ -10,6 +10,7 @@ use sysinfo::{CpuExt, System, SystemExt};
 async fn main() {
     let router = Router::new()
         .route("/", get(root_get))
+        .route("/index.js", get(indexjs_get))
         .route("/api/cpus", get(cpus_get))
         .with_state(AppState {
         sys: Arc::new(Mutex::new(System::new())),
@@ -34,6 +35,15 @@ struct AppState {
 async fn root_get() -> impl IntoResponse {
     let markup = tokio::fs::read_to_string("src/index.html").await.unwrap();
     Html(markup)
+}
+
+#[axum::debug_handler]
+async fn indexjs_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.js").await.unwrap();
+    Response::builder()
+        .header("content-type", "application/javascript;charset=utf-8")
+        .body(markup)
+        .unwrap()
 }
 
 #[axum::debug_handler]
